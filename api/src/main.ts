21c9b558436154af6d41f83636b2350341c404a2
pandoraxdn <pandoraxdn@protonmail.com>
2025-06-06 10:11:27 -0600

    05/06/2025

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'body-parser';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1");
+  app.use(json({ limit: "100mb" }));
+  app.use(urlencoded({ limit: "100mb", extended: true }));
   await app.listen(3000);
 }
 main();
